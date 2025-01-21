02806f3b7b3d4d2e0ecdc74b39110c791965a46b
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-01-21 18:44:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+	let pallindrom = "";
+	for(let i = 0 ; i<= str.length; i++){
+		palindrome = palindrome + str[i];
+	}
+	if(str === palindrome){
+		return true;
+	}
+	return false;
 
 }
 module.exports = palindrome
